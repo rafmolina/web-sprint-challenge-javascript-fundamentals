ddbd6f5f17c364fdc74c0a75bc871a4ed66b205e
Rafael Molina <seissaysseis@users.noreply.github.com>
2020-08-21 13:40:10 -0600

    counter i hate you

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,11 +23,11 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
 function summation(item){
-  function add() {
-    var counter = 0;
-    counter += 1;
-    return counter;
-  }
+  let counter = 0;
+  counter += 1;
+  return counter++;
 }
 console.log(summation(4))
